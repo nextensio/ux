95600ac164b6c3af941fd966c9dbc22d012a6958
Gopa Kumar <gopakumarce@gmail.com>
2020-10-29 22:55:42 -0400

    Re #6 Add a UI/UX page to input certificate information
    Forgot to add the certs rectJS code in the last checkin

---
+++ src/views/home/certs/CertsEdit.js
@@ -0,0 +1,108 @@
+import React, { lazy, useState, useEffect } from 'react'
+import {
+    CButton,
+    CCard,
+    CCardBody,
+    CCardGroup,
+    CCol,
+    CContainer,
+    CForm,
+    CInput,
+    CInputGroup,
+    CInputGroupPrepend,
+    CInputGroupText,
+    CRow,
+    CLabel,
+    CCardHeader,
+    CFormGroup,
+    CFormText,
+    CCardFooter,
+    CTextarea
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { withRouter } from 'react-router-dom';
+
+var common = require('../../../common')
+
+const CertsEdit = (props) => {
+    const initUserData = Object.freeze({
+        certid: "",
+        cert: []
+    });
+    const [userData, updateUserData] = useState(initUserData);
+
+    useEffect(() => {
+        if (typeof props.location.state != 'undefined') {
+            updateUserData(props.location.state)
+        }
+    }, []);
+
+    const handleChange = (e) => {
+        if (e.target.name == "cert") {
+            var target = e.target.value;
+        } else {
+            var target = e.target.value.trim();
+        }
+        updateUserData({
+            ...userData,
+            [e.target.name]: target
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        var ucode = userData.cert.split('').map(function (c) { return c.charCodeAt(0) });
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                certid: userData.certid,
+                cert: ucode
+            }),
+        };
+        fetch(common.api_href('/api/v1/addcert'), requestOptions)
+            .then(async response => {
+                const data = await response.json();
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    alert(error);
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+                // check for error response
+                if (data["Result"] != "ok") {
+                    alert(data["Result"])
+                } else {
+                    props.history.push('/home/certs/view')
+                }
+            })
+            .catch(error => {
+                alert('Error contacting server', error);
+            });
+    };
+
+    return (
+        <CCard>
+            <CCardHeader>
+                Add Policy
+            </CCardHeader>
+            <CCardBody>
+                <CForm>
+                    <CFormGroup>
+                        <CLabel htmlFor="nf-password">Certificate ID</CLabel>
+                        <CInput name="certid" placeholder={userData.certid} onChange={handleChange} />
+                    </CFormGroup>
+                    <CFormGroup>
+                        <CLabel htmlFor="nf-email">Certificate</CLabel>
+                        <CTextarea name="cert" placeholder={userData.cert} onChange={handleChange} />
+                    </CFormGroup>
+                </CForm>
+            </CCardBody>
+            <CCardFooter>
+                <CButton size="sm" color="primary" onClick={handleSubmit}>Submit</CButton>
+            </CCardFooter>
+        </CCard>
+    )
+}
+
+export default withRouter(CertsEdit)
---
+++ src/views/home/certs/CertsView.js
@@ -0,0 +1,217 @@
+import React, { lazy, useState, useEffect } from 'react'
+import {
+    CBadge,
+    CButton,
+    CButtonGroup,
+    CCard,
+    CCardBody,
+    CCollapse,
+    CCardFooter,
+    CCardHeader,
+    CCol,
+    CProgress,
+    CRow,
+    CCallout,
+    CDataTable,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { DocsLink } from 'src/reusable'
+import { withRouter } from 'react-router-dom';
+
+var common = require('../../../common')
+
+const fields = [
+    "certid",
+    {
+        key: 'edit',
+        label: '',
+        _style: { width: '1%' },
+        sorter: false,
+        filter: false
+    },
+    {
+        key: 'delete',
+        label: '',
+        _style: { width: '1%' },
+        sorter: false,
+        filter: false
+    },
+    {
+        key: 'show_details',
+        label: '',
+        _style: { width: '1%' },
+        sorter: false,
+        filter: false
+    }
+]
+
+
+const CertsView = (props) => {
+
+    const initTableData = Object.freeze(
+        []
+    );
+    const [usersData, updateUserData] = useState(initTableData);
+    const [details, setDetails] = useState([])
+
+    useEffect(() => {
+        setDetails([])
+        fetch(common.api_href('/api/v1/getallcerts'))
+            .then(response => response.json())
+            .then(data => {
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].hasOwnProperty('cert')) {
+                        data[i].cert = String.fromCharCode(...data[i].cert);
+                    }
+                }
+                updateUserData(data);
+            });
+    }, []);
+
+    const handleRefresh = (e) => {
+        setDetails([]);
+        fetch(common.api_href('/api/v1/getallcerts'))
+            .then(response => response.json())
+            .then(data => {
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].hasOwnProperty('cert')) {
+                        data[i].cert = String.fromCharCode(...data[i].cert);
+                    }
+                }
+                updateUserData(data);
+            });
+    }
+
+    const handleAdd = (e) => {
+        props.history.push('/home/certs/add')
+    }
+
+    const handleEdit = (index) => {
+        props.history.push({
+            pathname: '/home/certs/add',
+            state: usersData[index]
+        });
+        setDetails([])
+    }
+
+    const handleDelete = (index) => {
+        setDetails([])
+        fetch(common.api_href('/api/v1/delcert/') + usersData[index].certid)
+            .then(async response => {
+                const data = await response.json();
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    alert(error);
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+                // check for error response
+                if (data["Result"] != "ok") {
+                    alert(data["Result"])
+                }
+            })
+            .catch(error => {
+                alert('Error contacting server', error);
+            });
+    }
+
+    const toggleDetails = (index) => {
+        const position = details.indexOf(index)
+        let newDetails = details.slice()
+        if (position !== -1) {
+            newDetails.splice(position, 1)
+        } else {
+            newDetails = [...details, index]
+        }
+        setDetails(newDetails)
+    }
+
+    return (
+        <>
+            <CRow>
+                <CCol xs="24" lg="12">
+                    <CCard>
+                        <CCardHeader>
+                            Policies
+                  <DocsLink name="CModal" />
+                        </CCardHeader>
+                        <CCardBody>
+                            <CDataTable
+                                items={usersData}
+                                fields={fields}
+                                itemsPerPage={15}
+                                pagination
+                                scopedSlots={{
+                                    'edit':
+                                        (item, index) => {
+                                            return (
+                                                <td className="py-2">
+                                                    <CButton
+                                                        color="primary"
+                                                        variant="outline"
+                                                        shape="square"
+                                                        size="sm"
+                                                        onClick={() => { handleEdit(index) }}
+                                                    >
+                                                        Edit
+                                            </CButton>
+                                                </td>
+                                            )
+                                        },
+                                    'delete':
+                                        (item, index) => {
+                                            return (
+                                                <td className="py-2">
+                                                    <CButton
+                                                        color="primary"
+                                                        variant="outline"
+                                                        shape="square"
+                                                        size="sm"
+                                                        onClick={() => { handleDelete(index) }}
+                                                    >
+                                                        Delete
+                                            </CButton>
+                                                </td>
+                                            )
+                                        },
+                                    'show_details':
+                                        (item, index) => {
+                                            return (
+                                                <td className="py-2">
+                                                    <CButton
+                                                        color="primary"
+                                                        variant="outline"
+                                                        shape="square"
+                                                        size="sm"
+                                                        onClick={() => { toggleDetails(index) }}
+                                                    >
+                                                        {details.includes(index) ? 'Hide' : 'Show'}
+                                                    </CButton>
+                                                </td>
+                                            )
+                                        },
+                                    'details':
+                                        (item, index) => {
+                                            return (
+                                                <CCollapse show={details.includes(index)}>
+                                                    <CCardBody>
+                                                        <pre>
+                                                            {item.cert}
+                                                        </pre>
+                                                    </CCardBody>
+                                                </CCollapse>
+                                            )
+                                        }
+                                }}
+                            />
+                        </CCardBody>
+                    </CCard>
+                </CCol>
+            </CRow>
+            <CButton size="large" color="primary" onClick={handleRefresh}>Refresh</CButton>
+            <CButton size="large" color="secondary" onClick={handleAdd}>Add</CButton>
+        </>
+    )
+}
+
+export default withRouter(CertsView)
