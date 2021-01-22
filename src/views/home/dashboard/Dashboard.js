2c174991e566c3d8bc428617392d7a87eae4df7f
matt_l <ml6492@nyu.edu>
2021-01-22 13:58:47 -0800

    changes to UX controller interface

---
+++ src/views/home/dashboard/Dashboard.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import {
+  CWidgetDropdown,
+  CRow,
+  CCol,
+  CDropdown,
+  CDropdownMenu,
+  CDropdownItem,
+  CDropdownToggle
+} from '@coreui/react'
+
+import { CIcon } from '@coreui/icons-react'
+import ChartLineSimple from '../charts/ChartLineSimple'
+
+const Dashboard = () => {
+    // render
+    return (
+      <CRow>
+        <CCol sm="12" lg="6">
+          <CWidgetDropdown
+            color="gradient-info"
+            className="shadow"
+            header="9.823"
+            text="Gateways Online"
+            footerSlot={
+              <ChartLineSimple
+                pointed
+                className="mt-3 mx-3"
+                style={{height: '70px'}}
+                dataPoints={[1, 18, 9, 17, 34, 22, 11]}
+                pointHoverBackgroundColor="info"
+                options={{ elements: { line: { tension: 0.00001 }}}}
+                label="Members"
+                labels="months"
+              />
+            }
+          >
+            <CDropdown>
+              <CDropdownToggle color="transparent">
+                <CIcon name="cil-settings"/>
+              </CDropdownToggle>
+              <CDropdownMenu className="pt-0" placement="bottom-end">
+                <CDropdownItem>Go to Gateway</CDropdownItem>
+              </CDropdownMenu>
+            </CDropdown>
+          </CWidgetDropdown>
+        </CCol>
+  
+        <CCol sm="12" lg="6">
+          <CWidgetDropdown
+            color="gradient-warning"
+            className="shadow"
+            header="9.823"
+            text="Tenants active"
+            footerSlot={
+              <ChartLineSimple
+                pointed
+                className="mt-3 mx-3"
+                style={{height: '70px'}}
+                dataPoints={[78, 81, 80, 45, 34, 12, 40]}
+                options={{ elements: { line: { tension: 0.00001 }}}}
+                label="Members"
+                labels="months"
+              />
+            }
+          >
+            <CDropdown>
+              <CDropdownToggle color="transparent">
+                <CIcon name="cil-settings"/>
+              </CDropdownToggle>
+              <CDropdownMenu className="pt-0" placement="bottom-end">
+                <CDropdownItem>Go to Tenants</CDropdownItem>
+              </CDropdownMenu>
+            </CDropdown>
+          </CWidgetDropdown>
+        </CCol>
+    </CRow>
+    )
+}
+
+export default Dashboard
---
+++ src/views/tenant/TenantDashboard.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import {
+  CCard,
+  CCardHeader,
+  CCardBody,
+  CWidgetProgressIcon,
+  CRow,
+  CCol,
+} from '@coreui/react'
+
+import { CIcon } from '@coreui/icons-react'
+import { withRouter } from 'react-router-dom';
+
+const TenantDashboard = (props) => {
+    // render
+    return (
+      <>
+        <CRow className='justify-content-center'>
+          <CCol sm='24' md='10'>
+            <CCard 
+              accentColor='primary'
+              className='d-flex shadow-lg'
+            >
+              <CCardHeader>
+                <strong>Welcome to {props.match.params.id}'s Dashboard</strong>
+              </CCardHeader>
+              <CCardBody>
+                <CRow>
+                  <CCol sm='12' md='4'>
+                    <CWidgetProgressIcon
+                      header='1.889'
+                      text='Users'
+                      color='gradient-info'
+                      inverse
+                    >
+                      <CIcon
+                        name='cil-group'
+                        height='36'
+                      />
+                    </CWidgetProgressIcon>
+                  </CCol>
+                  <CCol sm='12' md='4'>
+                    <CWidgetProgressIcon
+                      header='1.889'
+                      text='Bundles Active'
+                      color='gradient-success'
+                      inverse
+                    >
+                      <CIcon
+                        name='cil-notes'
+                        height='36'
+                      />
+                    </CWidgetProgressIcon>
+                  </CCol>
+                  <CCol sm='12' md='4'>
+                    <CWidgetProgressIcon
+                      header='1.889'
+                      text='Routers Online'
+                      color='gradient-warning'
+                      inverse
+                    >
+                      <CIcon
+                        name='cil-router'
+                        height='36'
+                      />
+                    </CWidgetProgressIcon>
+                  </CCol>
+                </CRow>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </>
+    )
+}
+
+export default withRouter(TenantDashboard)
