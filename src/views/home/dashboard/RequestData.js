14ffa9f027d2b24b49f49286baf17130c056bf6c
matt_l <ml6492@nyu.edu>
2021-07-21 15:24:04 -0700

    Deleted all dashboard stuff for superadmin

--- src/views/home/dashboard/RequestData.js
+++
@@ -1,8 +0,0 @@
-const requestData = [
-    {id: 0, user: 'Mark', date: '04/19/21 04:09:12 PM', request: 'Please unblock Zoom'},
-    {id: 1, user: 'Amanda', date: '04/19/21 04:09:12 PM', request: 'Slow internet'},
-    {id: 2, user: 'Bill', date: '04/19/21 04:09:12 PM', request: 'Decrease bandwith'},
-    {id: 3, user: 'Jamie', date: '04/19/21 04:09:12 PM', request: 'Please unblock VSC'},
-  ]
-
-export default (requestData)
